bdf90c0fdaa602f4c996a60d5236d1819c622365
Mariko Anderson <marikoa@uw.edu>
2020-09-04 15:53:39 -0700

    remove test url

--- src/api.js
+++ src/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const client = axios.create({
     baseURL: 'https://outside-in-dev-api.herokuapp.com/1oZrs5FhN6k89xHf0HZYkRecNoUneok6'
-    // baseURL: 'https://outside-in-dev-api.herokuapp.com/1oZrs5FhN6k89xHf0HZYkRecNoUneok6BAD'
-
 });
 const api = {
     loadRestaurants() {
